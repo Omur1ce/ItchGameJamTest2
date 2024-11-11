a97a0b5d4d2232411434e6eb764609ba04ba2d9d
InkSansDEMON <yimanchan99@gmail.com>
2024-11-11 02:14:32 +0000

    Revert "Added Soundtrack"
    This reverts commit bcec7757fb02a14f9686269b8f63b1fb210255a2.

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
-
 {
-    public SoundtrackManager soundtrackManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void PlayGame()
     {
         // Load the next scene in the build order or specify by name
         SceneManager.LoadScene("LevelScene");  // Replace "GameScene" with your game scene name
-        soundtrackManager.PlayGameplayMusic();
     }
 
     public void QuitGame()
